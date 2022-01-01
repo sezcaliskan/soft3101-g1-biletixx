d2dd1f5f04e00191c5205024fc3c91c13f2c86a0
Sezin Elicaliskan <sezinelicaliskan@hotmail.com>
2022-01-02 00:09:05 +0300

    update registration

--- biletixx_website/accounts/forms.py
+++ biletixx_website/accounts/forms.py
@@ -7,16 +7,21 @@ from accounts.models import RegisteredUser, User
 
 
 class EventHolderSignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30)
+    last_name = forms.CharField(max_length=30)
     email = forms.EmailField(required=True)
+    
     
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = ("username", "email")
+        fields = ("username", "first_name", "last_name", "email")
 
 
     def save(self, commit=True):
         user = super().save(commit=False)
         user.is_eventholder = True
+        user.first_name = self.cleaned_data["first_name"]
+        user.last_name = self.cleaned_data["last_name"]
         user.email = self.cleaned_data["email"]
         if commit:
             user.save()
@@ -27,12 +32,14 @@ class RegisteredUserSignUpForm(UserCreationForm):
     fields = ("username", "email", "password")
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = ("username", "email")
+        fields = ("username","first_name", "last_name", "email")
 
     @transaction.atomic
     def save(self):
         user = super().save(commit=False)
         user.is_registereduser = True
+        user.first_name = self.cleaned_data["first_name"]
+        user.last_name = self.cleaned_data["last_name"]
         user.email = self.cleaned_data["email"]
         user.save()
         registereduser = RegisteredUser.objects.create(user=user)
